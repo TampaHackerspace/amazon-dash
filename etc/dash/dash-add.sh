3671e27985c597426d1f61e55d7d2de3e0e4eccc
Kevin Rogers <kevin@kevinrogers.org>
2016-08-09 02:00:30 -0400

    Modified README and renamed skeleton directory

--- etc/dash/dash-add.sh
+++ etc/dash/dash-add.sh
@@ -6,6 +6,6 @@ MAC=$( echo $1 | sed 's/[:-]//g' | tr '[:upper:]' '[:lower:]')
 
 echo Adding configuration for $MAC ...
 
-cp -r mac-00112233445566.d mac-$MAC.d
+cp -r mac-skeleton.d mac-$MAC.d
 
 echo Configuration folder added at `pwd`/mac-$MAC.d
